a70d6076f255028e631fe11c34674220bc90d2c5
MathiasOliverNielsen <math324e@elev.techcollege.dk>
2026-01-19 10:54:28 +0100

    Refactor App component to fetch and display random jokes; update styles for better layout and readability

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface Joke {
+  id: number;
+  type: string;
+  setup: string;
+  punchline: string;
 }
 
-export default App
+export function App() {
+  const [data, setData] = useState<Joke>();
+  // Url til at lave et fetch på
+  const url = "https://official-joke-api.appspot.com/random_joke";
+
+  // UseEffect hook der kun kører når komponentet mounter
+  useEffect(() => {
+    // Fetch funktion der kun kører én gang inde i useEffect
+    async function doFetchOnMount() {
+      const res = await fetch(url);
+      const data = await res.json();
+      setData(data);
+    }
+    doFetchOnMount();
+  }, []);
+
+  return (
+    <div>
+      <h1>Random Joke</h1>
+      {data && (
+        <div>
+          <p>
+            <strong>Setup:</strong> {data.setup}
+          </p>
+          <p>
+            <strong>Punchline:</strong> {data.punchline}
+          </p>
+          <button onClick={() => window.location.reload()}>Get New Joke</button>
+        </div>
+      )}
+    </div>
+  );
+}
